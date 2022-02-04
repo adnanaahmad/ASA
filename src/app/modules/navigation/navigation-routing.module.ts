90272601e7180ca646b5297f834e76edb001f8c3
adnanahmed-alivia <adnanahmed@aliviaanalytics.com>
2022-02-04 15:07:33 +0500

    integrity suite screen in progress

--- src/app/modules/navigation/navigation-routing.module.ts
+++ src/app/modules/navigation/navigation-routing.module.ts
@@ -7,14 +7,14 @@ const routes: Routes = [
     path: '',
     component: NavigationComponent,
     children: [
-      { path: 'integrity-suite', loadChildren: () => import('../integrity-suite/integrity-suite.module').then(m => m.IntegritySuiteModule) },
-      { path: 'organization', loadChildren: () => import('../organization/organization.module').then(m => m.OrganizationModule) },
-      { path: 'tenant', loadChildren: () => import('../tenant/tenant.module').then(m => m.TenantModule) },
-      { path: 'department', loadChildren: () => import('../department/department.module').then(m => m.DepartmentModule) },
-      { path: 'user-group', loadChildren: () => import('../user-group/user-group.module').then(m => m.UserGroupModule) },
-      { path: 'user', loadChildren: () => import('../user/user.module').then(m => m.UserModule) },
-      { path: 'security-policy', loadChildren: () => import('../security-policy/security-policy.module').then(m => m.SecurityPolicyModule) },
-      { path: 'alivia-services', loadChildren: () => import('../alivia-services/alivia-services.module').then(m => m.AliviaServicesModule) },
+      { path: 'integrity-suite', loadChildren: () => import('../features/integrity-suite/integrity-suite.module').then(m => m.IntegritySuiteModule) },
+      { path: 'organization', loadChildren: () => import('../features/organization/organization.module').then(m => m.OrganizationModule) },
+      { path: 'tenant', loadChildren: () => import('../features/tenant/tenant.module').then(m => m.TenantModule) },
+      { path: 'department', loadChildren: () => import('../features/department/department.module').then(m => m.DepartmentModule) },
+      { path: 'user-group', loadChildren: () => import('../features/user-group/user-group.module').then(m => m.UserGroupModule) },
+      { path: 'user', loadChildren: () => import('../features/user/user.module').then(m => m.UserModule) },
+      { path: 'security-policy', loadChildren: () => import('../features/security-policy/security-policy.module').then(m => m.SecurityPolicyModule) },
+      { path: 'alivia-services', loadChildren: () => import('../features/alivia-services/alivia-services.module').then(m => m.AliviaServicesModule) },
     ]
   },
 ];
