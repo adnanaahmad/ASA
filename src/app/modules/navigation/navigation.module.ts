6dd669c80981f5e6489cc9c0779903d72f4034d0
adnanahmed-alivia <adnanahmed@aliviaanalytics.com>
2022-02-07 14:30:21 +0500

    AA-505 unused components removed

--- src/app/modules/navigation/navigation.module.ts
+++ src/app/modules/navigation/navigation.module.ts
@@ -1,15 +1,11 @@
 import { NgModule } from '@angular/core';
 import { NavigationRoutingModule } from './navigation-routing.module';
-import { SideNavComponent } from './components/side-nav/side-nav.component';
-import { FixedNavComponent } from './components/fixed-nav/fixed-nav.component';
 import { NavigationComponent } from './components/navigation/navigation.component';
 import { MutualModule } from 'src/app/mutual.module';
 
 
 @NgModule({
   declarations: [
-    SideNavComponent,
-    FixedNavComponent,
     NavigationComponent
   ],
   imports: [
