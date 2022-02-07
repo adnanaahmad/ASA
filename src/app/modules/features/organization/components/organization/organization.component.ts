c73d065b32ad230512d435d135daf642688af1c9
adnanahmed-alivia <adnanahmed@aliviaanalytics.com>
2022-02-07 15:21:30 +0500

    AA-505 organization module in progress

--- src/app/modules/features/organization/components/organization/organization.component.ts
+++ src/app/modules/features/organization/components/organization/organization.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./organization.component.scss']
 })
 export class OrganizationComponent implements OnInit {
-
+  hide: boolean = true;
   constructor() { }
 
   ngOnInit(): void {
