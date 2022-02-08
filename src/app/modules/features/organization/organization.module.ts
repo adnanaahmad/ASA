04bfe20461b3a70cf0b1ff3fd64c4a5a1527559e
adnanahmed-alivia <adnanahmed@aliviaanalytics.com>
2022-02-08 17:38:54 +0500

    AA-505 hierarchy in progress

--- src/app/modules/features/organization/organization.module.ts
+++ src/app/modules/features/organization/organization.module.ts
@@ -5,6 +5,7 @@ import { MutualModule } from 'src/app/mutual.module';
 import { BasicInfoComponent } from './components/basic-info/basic-info.component';
 import { ProductAccessComponent } from './components/product-access/product-access.component';
 import { HierarchyComponent } from './components/hierarchy/hierarchy.component';
+import {SharedModule} from "../../../shared/shared.module";
 
 @NgModule({
   declarations: [
@@ -13,9 +14,10 @@ import { HierarchyComponent } from './components/hierarchy/hierarchy.component';
     ProductAccessComponent,
     HierarchyComponent
   ],
-  imports: [
-    MutualModule,
-    OrganizationRoutingModule
-  ]
+    imports: [
+        MutualModule,
+        OrganizationRoutingModule,
+        SharedModule
+    ]
 })
 export class OrganizationModule { }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,52 +1,20 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 import {SharedRoutingModule} from "./shared-routing.module";
-import {MatCardModule} from '@angular/material/card';
-import {MatButtonModule} from '@angular/material/button';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatAutocompleteModule} from '@angular/material/autocomplete';
-import {MatBadgeModule} from '@angular/material/badge';
-import {MatBottomSheetModule} from '@angular/material/bottom-sheet';
-import {MatButtonToggleModule} from '@angular/material/button-toggle';
-import {MatCheckboxModule} from '@angular/material/checkbox';
-import {MatChipsModule} from '@angular/material/chips';
-import {MatCommonModule} from '@angular/material/core';
-import {MatDatepickerModule} from '@angular/material/datepicker';
-import {MatDialogModule} from '@angular/material/dialog';
-import {MatDividerModule} from '@angular/material/divider';
-import {MatExpansionModule} from '@angular/material/expansion';
-import {MatGridListModule} from '@angular/material/grid-list';
-import {MatIconModule} from '@angular/material/icon';
-import {MatInputModule} from '@angular/material/input';
-import {MatListModule} from '@angular/material/list';
-import {MatMenuModule} from '@angular/material/menu';
-import {MatPaginatorModule} from '@angular/material/paginator';
-import {MatProgressBarModule} from '@angular/material/progress-bar';
-import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
-import {MatRadioModule} from '@angular/material/radio';
-import {MatRippleModule} from '@angular/material/core';
-import {MatSelectModule} from '@angular/material/select';
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatSlideToggleModule} from '@angular/material/slide-toggle';
-import {MatSliderModule} from '@angular/material/slider';
-import {MatSnackBarModule} from '@angular/material/snack-bar';
-import {MatSortModule} from '@angular/material/sort';
-import {MatStepperModule} from '@angular/material/stepper';
-import {MatTableModule} from '@angular/material/table';
-import {MatTabsModule} from '@angular/material/tabs';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatTooltipModule} from '@angular/material/tooltip';
-import {MatTreeModule} from '@angular/material/tree';
-import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import { TableComponent } from './components/table/table.component';
+import {MutualModule} from "../mutual.module";
 
 @NgModule({
   declarations: [
-    PageNotFoundComponent
+    PageNotFoundComponent,
+    TableComponent
+  ],
+  exports: [
+    TableComponent
   ],
   imports: [
-    CommonModule,
     SharedRoutingModule,
+    MutualModule
   ]
 })
 export class SharedModule { }
