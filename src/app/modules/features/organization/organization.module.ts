9b086fa26e88d0874d8c83bcd696338e1f0a03db
adnanahmed-alivia <adnanahmed@aliviaanalytics.com>
2022-02-08 18:40:41 +0500

    AA-505 policies user management added

--- src/app/modules/features/organization/organization.module.ts
+++ src/app/modules/features/organization/organization.module.ts
@@ -5,13 +5,17 @@ import { BasicInfoComponent } from './components/basic-info/basic-info.component
 import { ProductAccessComponent } from './components/product-access/product-access.component';
 import { HierarchyComponent } from './components/hierarchy/hierarchy.component';
 import {SharedModule} from "../../../shared/shared.module";
+import { PoliciesComponent } from './components/policies/policies.component';
+import { UserManagementComponent } from './components/user-management/user-management.component';
 
 @NgModule({
   declarations: [
     OrganizationComponent,
     BasicInfoComponent,
     ProductAccessComponent,
-    HierarchyComponent
+    HierarchyComponent,
+    PoliciesComponent,
+    UserManagementComponent
   ],
     imports: [
       SharedModule,
