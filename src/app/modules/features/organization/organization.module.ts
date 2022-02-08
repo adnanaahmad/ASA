41f7ef509d8a8a168515339ae771621b386ca569
adnanahmed-alivia <adnanahmed@aliviaanalytics.com>
2022-02-08 15:21:02 +0500

    AA-505 hierarchy in progress

--- src/app/modules/features/organization/organization.module.ts
+++ src/app/modules/features/organization/organization.module.ts
@@ -4,12 +4,14 @@ import { OrganizationComponent } from './components/organization/organization.co
 import { MutualModule } from 'src/app/mutual.module';
 import { BasicInfoComponent } from './components/basic-info/basic-info.component';
 import { ProductAccessComponent } from './components/product-access/product-access.component';
+import { HierarchyComponent } from './components/hierarchy/hierarchy.component';
 
 @NgModule({
   declarations: [
     OrganizationComponent,
     BasicInfoComponent,
-    ProductAccessComponent
+    ProductAccessComponent,
+    HierarchyComponent
   ],
   imports: [
     MutualModule,
