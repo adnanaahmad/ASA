7e0f095eec3ca82216073788039df3b93ea296d7
adnanahmed-alivia <adnanahmed@aliviaanalytics.com>
2022-02-17 13:14:10 +0500

    AA-505 theme applied back to body atm

--- src/app/shared/services/core/theme/theme.service.ts
+++ src/app/shared/services/core/theme/theme.service.ts
@@ -8,12 +8,11 @@ export class ThemeService {
    * toggleDarkTheme() function applies dynamic theme only on app-navigation component
    * */
   toggleDarkTheme() {
-    let app = document.getElementsByTagName("app-navigation")[0];
     if (this.isDark) {
-      app.classList.remove('dark-theme');
+      document.body.classList.remove('dark-theme');
       this.isDark = false;
     } else {
-      app.classList.add('dark-theme');
+      document.body.classList.add('dark-theme');
       this.isDark = true;
     }
   }
