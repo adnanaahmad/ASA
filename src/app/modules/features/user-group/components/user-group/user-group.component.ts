800f5653ffb8a1414f1de04e73861efb5e137f5c
adnanahmed-alivia <adnanahmed@aliviaanalytics.com>
2022-02-18 16:39:17 +0500

    AA=505 user group table done

--- src/app/modules/features/user-group/components/user-group/user-group.component.ts
+++ src/app/modules/features/user-group/components/user-group/user-group.component.ts
@@ -9,7 +9,15 @@ import {AddUserGroupDialogComponent} from "../add-user-group-dialog/add-user-gro
   styleUrls: ['./user-group.component.scss']
 })
 export class UserGroupComponent implements OnInit {
-
+  tableData: Array<any> = [
+    {row: '1', userGroup: 'Group 5', description: 'this is the finance dept', products: 'FWA', department: 'Finance', dataMasking: 'Enabled', defaultLandingPage: 'Ranking', view: '', edit: '', remove: '', active: '' },
+    {row: '2', userGroup: 'Group 5', description: 'this is the finance dept', products: 'FWA', department: 'Finance', dataMasking: 'Enabled', defaultLandingPage: 'Ranking', view: '', edit: '', remove: '', active: '' },
+    {row: '3', userGroup: 'Group 5', description: 'this is the finance dept', products: 'FWA', department: 'Finance', dataMasking: 'Enabled', defaultLandingPage: 'Ranking', view: '', edit: '', remove: '', active: '' },
+    {row: '4', userGroup: 'Group 5', description: 'this is the finance dept', products: 'FWA', department: 'Finance', dataMasking: 'Enabled', defaultLandingPage: 'Ranking', view: '', edit: '', remove: '', active: '' },
+    {row: '5', userGroup: 'Group 5', description: 'this is the finance dept', products: 'FWA', department: 'Finance', dataMasking: 'Enabled', defaultLandingPage: 'Ranking', view: '', edit: '', remove: '', active: '' },
+    {row: '6', userGroup: 'Group 5', description: 'this is the finance dept', products: 'FWA', department: 'Finance', dataMasking: 'Enabled', defaultLandingPage: 'Ranking', view: '', edit: '', remove: '', active: '' },
+  ];
+  tableColumns: Array<string> = ['row', 'userGroup', 'description', 'products', 'department', 'dataMasking', 'defaultLandingPage', 'view', 'edit', 'remove', 'active'];
   constructor(public borderService: ConditionalBorderService, public dialog: MatDialog) { }
 
   ngOnInit(): void {
