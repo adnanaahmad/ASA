58de6b615f1761d6b82f9531007921d777242c40
adnanahmed-alivia <adnanahmed@aliviaanalytics.com>
2022-02-18 16:28:03 +0500

    AA=505 table for dept done

--- src/app/modules/features/department/components/department/department.component.ts
+++ src/app/modules/features/department/components/department/department.component.ts
@@ -9,7 +9,15 @@ import {AddDepartmentDialogComponent} from "../add-department-dialog/add-departm
   styleUrls: ['./department.component.scss']
 })
 export class DepartmentComponent implements OnInit {
-
+  tableData: Array<any> = [
+    {row: '1', department: 'Finance', description: 'this is the finance dept', products: 'FWA', tenant: 'Advise Health', organization: 'Claim Secure', view: '', edit: '', remove: '', active: '' },
+    {row: '2', department: 'Finance', description: 'this is the finance dept', products: 'FWA', tenant: 'Advise Health', organization: 'Claim Secure', view: '', edit: '', remove: '', active: '' },
+    {row: '3', department: 'Finance', description: 'this is the finance dept', products: 'FWA', tenant: 'Advise Health', organization: 'Claim Secure', view: '', edit: '', remove: '', active: '' },
+    {row: '4', department: 'Finance', description: 'this is the finance dept', products: 'FWA', tenant: 'Advise Health', organization: 'Claim Secure', view: '', edit: '', remove: '', active: '' },
+    {row: '5', department: 'Finance', description: 'this is the finance dept', products: 'FWA', tenant: 'Advise Health', organization: 'Claim Secure', view: '', edit: '', remove: '', active: '' },
+    {row: '6', department: 'Finance', description: 'this is the finance dept', products: 'FWA', tenant: 'Advise Health', organization: 'Claim Secure', view: '', edit: '', remove: '', active: '' },
+  ];
+  tableColumns: Array<string> = ['row', 'department', 'description', 'products', 'tenant', 'organization', 'view', 'edit', 'remove', 'active'];
   constructor(public borderService: ConditionalBorderService, public dialog: MatDialog) { }
 
   ngOnInit(): void {
