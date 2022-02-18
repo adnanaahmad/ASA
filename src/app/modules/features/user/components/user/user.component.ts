727e81a1370c275135836136c08594a181181e87
adnanahmed-alivia <adnanahmed@aliviaanalytics.com>
2022-02-18 16:49:08 +0500

    AA-505 user table done

--- src/app/modules/features/user/components/user/user.component.ts
+++ src/app/modules/features/user/components/user/user.component.ts
@@ -9,7 +9,14 @@ import {AddUserDialogComponent} from "../add-user-dialog/add-user-dialog.compone
   styleUrls: ['./user.component.scss']
 })
 export class UserComponent implements OnInit {
-
+  tableData: Array<any> = [
+    {row: '1', name: 'lewis', userName: 'lewis123', userGroup: 'Group 5', department: 'Finance', tenant: 'Advice Health', privileges: 'Organization Level Admin', view: '', edit: '', remove: '', active: '' },
+    {row: '2', name: 'lewis', userName: 'lewis123', userGroup: 'Group 5', department: 'Finance', tenant: 'Advice Health', privileges: 'Organization Level Admin', view: '', edit: '', remove: '', active: '' },
+    {row: '3', name: 'lewis', userName: 'lewis123', userGroup: 'Group 5', department: 'Finance', tenant: 'Advice Health', privileges: 'Organization Level Admin', view: '', edit: '', remove: '', active: '' },
+    {row: '4', name: 'lewis', userName: 'lewis123', userGroup: 'Group 5', department: 'Finance', tenant: 'Advice Health', privileges: 'Organization Level Admin', view: '', edit: '', remove: '', active: '' },
+    {row: '5', name: 'lewis', userName: 'lewis123', userGroup: 'Group 5', department: 'Finance', tenant: 'Advice Health', privileges: 'Organization Level Admin', view: '', edit: '', remove: '', active: '' },
+  ];
+  tableColumns: Array<string> = ['row','name', 'userName', 'userGroup', 'department', 'tenant', 'privileges', 'view', 'edit', 'remove', 'active'];
   constructor(public borderService: ConditionalBorderService, public dialog: MatDialog) { }
 
   ngOnInit(): void {
